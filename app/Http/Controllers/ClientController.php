a9d8be420ddeff15982644b0d5dee22a11a49801
Jefferson L Ferreira <jeffersonluan@gmail.com>
2024-09-27 08:52:51 -0300

    Módulo 2 - Integrando a OpenAI

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Client;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\StoreClientRequest;
+use App\Http\Requests\UpdateClientRequest;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $clients = Client::with('user', 'address')->paginate(10);
+
+        return view('clients.index', compact('clients'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('clients.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreClientRequest $request)
+    {
+        DB::transaction(function () use($request) {
+            $user = User::create([
+                'email' => $request->get('email'),
+                'name' => $request->get('name'),
+                'password' => Hash::make('123456789')
+            ]);
+
+            $user->client()->create([
+                'address_id' => $request->get('address_id'),
+            ]);
+        });
+
+        return redirect()->route('clients.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Client $client)
+    {
+        return view('clients.edit', compact('client'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateClientRequest $request, Client $client)
+    {
+        DB::transaction(function () use($request, $client) {
+            $client->user->update([
+                'email' => $request->get('email'),
+                'name' => $request->get('name')
+            ]);
+
+            $client->update([
+                'address_id' => $request->get('address_id')
+            ]);
+        });
+
+        return redirect()->route('clients.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Client $client)
+    {
+        if ($client) {
+
+            $client->delete(); // Aplica o Soft Delete
+
+            return redirect()->back()->with('success', 'Cliente excluído com sucesso.');
+
+        } else {
+
+            return redirect()->back()->with('error', 'Cliente não encontrado.');
+
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
+use App\Models\SalesCommission;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProfileController;
+use OpenAI\Laravel\Facades\OpenAI;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/clients', ClientController::class);
+
+    Route::get('/chart', function () {
+        $fields = implode(',', SalesCommission::getColumns());
+        $question = 'Gere um grafico das vendas por empresa no eixo y ao logo dos ultimos 5 anos';
+
+        $config = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => "Considerando a lista de campos ($fields), gere uma configuração json do vega-lite v5 (sem campo de dados e com descricao) que atenda o seguinte pedido $question. Resposta:",
+            'max_tokens' => 1500
+        ])->choices[0]->message->content;
+
+        dd($config);
+    });
+});
+
+require __DIR__.'/auth.php';
