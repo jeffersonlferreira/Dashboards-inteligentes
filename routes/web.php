f6e6d7883f0bce82cb6eb7c5bbebaea5d5cede9d
Jefferson L Ferreira <jeffersonluan@gmail.com>
2024-09-30 16:10:00 -0300

    Módulo 3 - Frontend Dinâmico no PHP com Livewire

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Livewire\Dashboard;
 use App\Models\SalesCommission;
+use OpenAI\Laravel\Facades\OpenAI;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SaleController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
-use OpenAI\Laravel\Facades\OpenAI;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', Dashboard::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -20,19 +20,20 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('/clients', ClientController::class);
+    Route::get('/sales', [SaleController::class, 'index'])->name('sales.index');
 
-    Route::get('/chart', function () {
-        $fields = implode(',', SalesCommission::getColumns());
-        $question = 'Gere um grafico das vendas por empresa no eixo y ao logo dos ultimos 5 anos';
+    // Route::get('/chart', function () {
+    //     $fields = implode(',', SalesCommission::getColumns());
+    //     $question = 'Gere um grafico das vendas por empresa no eixo y ao logo dos ultimos 5 anos';
 
-        $config = OpenAI::chat()->create([
-            'model' => 'gpt-3.5-turbo',
-            'messages' => "Considerando a lista de campos ($fields), gere uma configuração json do vega-lite v5 (sem campo de dados e com descricao) que atenda o seguinte pedido $question. Resposta:",
-            'max_tokens' => 1500
-        ])->choices[0]->message->content;
+    //     $config = OpenAI::chat()->create([
+    //         'model' => 'gpt-3.5-turbo',
+    //         'messages' => "Considerando a lista de campos ($fields), gere uma configuração json do vega-lite v5 (sem campo de dados e com descricao) que atenda o seguinte pedido $question. Resposta:",
+    //         'max_tokens' => 1500
+    //     ])->choices[0]->message->content;
 
-        dd($config);
-    });
+    //     dd($config);
+    // });
 });
 
 require __DIR__.'/auth.php';
